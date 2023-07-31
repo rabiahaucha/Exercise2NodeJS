cbdf669c323a211ea51a31b4c8ff538dbdff5c68
rabiahaucha <rabiahaucha9@gmail.com>
2023-07-31 15:44:52 +0200

    done

---
+++ index.js
@@ -0,0 +1,9 @@
+const Person = require('./person')
+
+const Person1 = new Person ("Rabia","Haucha","rabiahaucha9@gmail.com");
+const Person2 = new Person ("Summer","De Wet","summerdewet@gmail.com");
+const Person3 = new Person ("Lithini","Skhikhi","lithiniskhihi9@gmail.com");
+
+console.log(Person1);
+console.log(Person2);
+console.log(Person3);
---
+++ person.js
@@ -0,0 +1,7 @@
+function Person (first,last,email){
+    this.first = first,
+     this.last = last,
+     this.email = email
+   };
+   
+module.exports = Person   
